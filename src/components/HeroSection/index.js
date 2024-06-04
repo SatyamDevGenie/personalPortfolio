65fb2042dab99dab8481134df211a540592bf81e
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-05 01:21:19 +0530

    my portfolio

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import { Bio } from "../../data/constants";
+import ProfileImg from "../../images/profile.jpg";
+import HeroBgAnimation from "../HeroBgAnimation";
+import {
+  HeroBg,
+  HeroContainer,
+  HeroInnerContainer,
+  HeroLeftContainer,
+  HeroRightContainer,
+  Img,
+  ResumeButton,
+  Span,
+  SubTitle,
+  TextLoop,
+  Title,
+} from "./HeroStyle";
+
+const HeroSection = () => {
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={ProfileImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default HeroSection;
