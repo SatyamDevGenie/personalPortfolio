bd5c524ddd5ee5d230ff24d0a04e2254953c0ca7
Your Name <your.email@example.com>
2024-07-26 05:57:21 +0530

    update my experiencee

--- src/data/constants.js
+++ src/data/constants.js
@@ -169,12 +169,36 @@ export const skills = [
 
 export const experiences = [
   {
-    id: 0,
+    id: 1,
+    // img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Freelance Web Developer",
+    company: "Self Employed",
+    date: "Jan 2023 - Present",
+    desc: "Experienced freelance web developer specializing in MERN stack technologies. Over the past year, I've successfully delivered responsive and dynamic web applications for various clients, focusing on quality and client satisfaction.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Chakra UI",
+      "HTML",
+      "CSS",
+      "Tailwind",
+      "JavaScript",
+      "Postman",
+      "Linode",
+      "Devtools",
+      "MongoDB",
+      "Express",
+    ],
+  },
+
+  {
+    id: 1,
     // img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
     role: "Frontend End Developer",
     company: "Hi-Technic Systems and Services",
     date: "August 2023 - July 2024",
-    desc: "Working on the frontend of the web application using ReactJS, Redux, and Javascript.",
+    desc: "Experienced Frontend Developer skilled in React, Redux, and JavaScript, with a strong track record of building dynamic and responsive web applications. Proficient in creating user-friendly interfaces and implementing efficient state management.",
     skills: [
       "ReactJS",
       "Redux",
@@ -187,12 +211,12 @@ export const experiences = [
     // doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
   },
   {
-    id: 1,
+    id: 2,
     // img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
     role: "Full Stack Training",
     company: "Rst Forum",
     date: "Feb 2023 - June 2023",
-    desc: "Full Stack Training, where i build and work on ecommerce application. The entire web application is build under the rstforum.",
+    desc: "Full Stack Training, where i build and work on ecommerce application. The entire web application is build under the rstforum. Completed an intensive 5-month training program focused on the MERN stack (MongoDB, Express.js, React.js, Node.js). Acquired hands-on experience in building dynamic, responsive ecommerce web applications from front-end to back-end.",
     skills: [
       "ReactJS",
       "Redux",
